a2eff4d009881806bee1ed3e07e2c94e5a2ef80f
Joseph Garcia <prosif@gmail.com>
2018-07-08 20:38:38 -0700

    handle window resize

--- game_server.js
+++ game_server.js
@@ -4,30 +4,46 @@ const server = new WebSocket.Server({
 	port: 8080
 });
 
-let width = 128;
-let height = 720;
+const COLORS = {
+	BLACK: [0, 0, 0, 255],
+	WHITE: [255, 255, 255, 255],
+	GRAY: [190, 190, 190, 255],
+	RED: [255, 26, 9, 255],
+	PURPLE: [225, 26, 255, 255],
+	BLUE: [0, 9, 255, 255],
+	GREEN: [80, 211, 0, 255],
+	ORANGE: [234, 106, 56, 255]
+};
+
+let colorKeys = Object.keys(COLORS);
+
+// max "good" size seems like ~320x180
+
+let width = 160;
+let height = 90;
 let gamePixels = new Uint8ClampedArray(width * height * 4);
 
 setInterval(function() {
 	let newGamePixels = new Uint8ClampedArray(width * height * 4);
 	
 	for (let i = 0; i < gamePixels.length; i+=4) {
-		newGamePixels[i] = Math.floor(Math.random() * 255);;
-  	newGamePixels[i + 1] = Math.floor(Math.random() * 255);;
-  	newGamePixels[i + 2] = Math.floor(Math.random() * 255);;
-  	newGamePixels[i + 3] = 255;
+		let colorKey = colorKeys[Math.floor(Math.random() * colorKeys.length)];
+		newGamePixels[i] = COLORS[colorKey][0];
+		newGamePixels[i + 1] = COLORS[colorKey][1];
+		newGamePixels[i + 2] = COLORS[colorKey][2];
+		newGamePixels[i + 3] = COLORS[colorKey][3];
 	}
 	gamePixels = newGamePixels;
 }, 10);
 
+
 setInterval(function() {
 	server.clients.forEach(function(client) {
-		if (client.readyState === WebSocket.OPEN) {
-			client.send(gamePixels);
+  	if (client.readyState === WebSocket.OPEN) {
+    	client.send(gamePixels);
 		}
 	});
 }, 16.67);
-
 
 server.on('connection', (ws) => {
 //	console.log("ws connection", ws);
--- public/App.js
+++ public/App.js
@@ -13,100 +13,53 @@ socket.onopen = function(e) {
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext('2d');
 
-let scaledWidth = 1280;
-let scaledHeight = 720;
-let bytesPerPixel = 4;
-let tempPixels = new Uint8ClampedArray(scaledWidth * scaledHeight * bytesPerPixel);
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+
+canvas.height = windowHeight;
+canvas.width = windowWidth;
+
+let originalWidth = 160;
+let originalHeight = 90;
+let scaleFactor = Math.floor(windowWidth / originalWidth);
+let BYTES_PER_PIXEL = 4;
+
+let tempPixels = new Uint8ClampedArray(originalWidth * originalHeight * scaleFactor * scaleFactor * BYTES_PER_PIXEL);
 
 let scalePixels = function(buff) {
 	let arr = new Uint8ClampedArray(buff);
 
-//	let pixels = [];
-//	for (let i = 0; i < arr.length; i+=4) {
-//		pixels.push(arr.slice(i, i+4));
-//	}
-	
-//	let newPixels2D = new Array(scaledWidth);
-//	for (let i = 0; i < scaledWidth; i++) {
-//		newPixels2D[i] = new Array(scaledHeight);
-//	}
-//	for (let i = 0; i < pixels.length; i++) {	
-//		startX = 10 * Math.floor(i / 100);
-//		startY = 10 * (i % 100);
-//		for (let x = startX; x < startX + 10; x++) {
-//			for (let y = startY; y < startY + 10; y++) {
-//				newPixels2D[x][y] = pixels[i];
-//			}
-//		}
-//	}				
-
-	//let newPixelsIndex = 0;
-	// For each pixel in source 1D array
-//	for (let i = 0; i < arr.length; i+=4) {
-//		let startX = 10 * (Math.floor( i/ 100));
-//		let startY = 10 * ( i % 100);
-//		for (let x = startX; x < startX + 10; x++) {
-//			for (let y = startY; y < startY + 10; y++) {
-//				console.log(newPixels2D[x][y]);
-//				let theNewIndex = 4 * (1000 * x) + 4 * y;
-//	  		tempPixels[theNewIndex] = newPixels2D[x][y][0];
-//	  		tempPixels[theNewIndex + 1] = newPixels2D[x][y][1];
-//	  		tempPixels[theNewIndex + 2] = newPixels2D[x][y][2];
-//	  		tempPixels[theNewIndex + 3] = newPixels2D[x][y][3];
-//				//tempPixels[newIndex] = arr[i];
-//				//tempPixels[newIndex + 1] = arr[i + 1];
-//				//tempPixels[newIndex + 2] = arr[i + 2];
-//				//tempPixels[newIndex + 3] = arr[i + 3];
-//			}
-//		}
-//	}
-	
-	for (let k = 0; k < arr.length; k+=4) {
-		let startX = 10 * Math.floor( (k/4) / 128);
-		let startY = 10 * ( (k/4) % 128);
-		for (let x = startX; x < startX + 10; x++) {
-			for (let y = startY; y < startY + 10; y++) {
-				let theNewIndex = 4 * (1280 * x) + 4 * y;
+	for (let k = 0; k < arr.length; k+=BYTES_PER_PIXEL) {
+		let startX = scaleFactor * Math.floor((k / BYTES_PER_PIXEL) / originalWidth);
+		let startY = scaleFactor * ((k / BYTES_PER_PIXEL) % originalWidth);
+		for (let x = startX; x < startX + scaleFactor; x++) {
+			for (let y = startY; y < startY + scaleFactor; y++) {
+				let theNewIndex = BYTES_PER_PIXEL * ((originalWidth * scaleFactor * x) + y);
 				tempPixels[theNewIndex] = arr[k];
 				tempPixels[theNewIndex + 1] = arr[k + 1];
 				tempPixels[theNewIndex + 2] = arr[k + 2];
 				tempPixels[theNewIndex + 3] = arr[k + 3];
-				//tempPixels[theNewIndex] = newPixels2D[x][y][0];
-				//tempPixels[theNewIndex + 1] = newPixels2D[x][y][1];
-				//tempPixels[theNewIndex + 2] = newPixels2D[x][y][2];
-				//tempPixels[theNewIndex + 3] = newPixels2D[x][y][3];
 			}
 		}
 	}
-//		let startX =  Math.floor((k/4) / 100); 
-//		let startY =  (k/4) % 100;
-//		for (let x = startX; x < startX + 10; x++) {
-//			for (let y = startY; y < startY + 10; y++) {
-//				let theNewIndex = (4 * (1000 * x)) + ( 4 * y);
-//				tempPixels[theNewIndex] = newPixels2D[x][y][0];
-//				tempPixels[theNewIndex + 1] = newPixels2D[x][y][1];
-//				tempPixels[theNewIndex + 2] = newPixels2D[x][y][2];
-//				tempPixels[theNewIndex + 3] = newPixels2D[x][y][3];
-//			}
-//		}
-//	}
-
-//	for (let i = 0; i < newPixels2D.length; i++) {
-//		for (let j = 0; j < newPixels2D[0].length; j++) {
-//			let theNewIndex = 4 * (1000 * i) + 4 * j;
-//			tempPixels[theNewIndex] = newPixels2D[i][j][0];
-//			tempPixels[theNewIndex + 1] = newPixels2D[i][j][1];
-//			tempPixels[theNewIndex + 2] = newPixels2D[i][j][2];
-//			tempPixels[theNewIndex + 3] = newPixels2D[i][j][3];
-//		}
-//	}
 
 	return tempPixels;
 };
 
 socket.onmessage = function(msg) {
 	let newPixels = scalePixels(msg.data);
-	let imageData = new ImageData(newPixels, 1280, 720);
-  ctx.clearRect(0, 0, 1280, 720);
+	let imageData = new ImageData(newPixels, originalWidth * scaleFactor, originalHeight * scaleFactor);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.putImageData(imageData, 0, 0);
 };
+
+window.addEventListener("resize", function(x) {
+	// todo: this should not interfere with current pixels being repainted
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+	scaleFactor = Math.floor(windowWidth / originalWidth);
+	tempPixels = new Uint8ClampedArray(originalWidth * originalHeight * scaleFactor * scaleFactor * BYTES_PER_PIXEL);
+	canvas.height = windowHeight;
+	canvas.width = windowWidth;
+});
+
