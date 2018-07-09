d8d7cef3455d95c05bf163644721bb72c8397b15
Joseph Garcia <prosif@gmail.com>
2018-07-08 17:24:14 -0700

    kind of working w/ scaling

--- game_server.js
+++ game_server.js
@@ -1,34 +1,40 @@
-const uuid = require('uuid/v4');
 const WebSocket = require('ws');
 
 const server = new WebSocket.Server({
 	port: 8080
 });
 
-let width = 240;
-let height = 135;
+let width = 10;
+let height = 10;
 let gamePixels = new Uint8ClampedArray(width * height * 4);
-for (let i = 0; i < gamePixels.length; i+=4) {
-	gamePixels[i] = 255;
-  gamePixels[i + 1] = 0;
-  gamePixels[i + 2] = 0;
-  gamePixels[i + 3] = 255;
-}
+//for (let i = 0; i < gamePixels.length; i+=4) {
+//	gamePixels[i] = 255;
+//  gamePixels[i + 1] = 0;
+//  gamePixels[i + 2] = 0;
+//  gamePixels[i + 3] = 255;
+//}
+
+setInterval(function() {
+	let newGamePixels = new Uint8ClampedArray(width * height * 4);
+	
+	for (let i = 0; i < gamePixels.length; i+=4) {
+		newGamePixels[i] = Math.floor(Math.random() * 255);;
+  	newGamePixels[i + 1] = Math.floor(Math.random() * 255);;
+  	newGamePixels[i + 2] = Math.floor(Math.random() * 255);;
+  	newGamePixels[i + 3] = 255;
+	}
+	gamePixels = newGamePixels;
+}, 10);
+
+setInterval(function() {
+	server.clients.forEach(function(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(gamePixels);
+		}
+	});
+}, 16.67);
+
 
 server.on('connection', (ws) => {
-
-	//connection.clientId = uuid();
-
-  //let message = {
-	//	"type": "id",
-	//	"id": connection.clientId
-	//};
-
-	//clients[connection.clientId] = connection;
-	
-	ws.on('message', function(message) {
-		ws.send(gamePixels.buffer);
-		// TODO: parse type. for now, assume requesting pixel state
-		//connection.send(JSON.stringify(gameState.buffer));
-	});
+//	console.log("ws connection", ws);
 });
--- public/App.js
+++ public/App.js
@@ -1,6 +1,6 @@
 let title = document.getElementById('title');
 
-const socket = new WebSocket('ws://127.0.0.1:8080');
+const socket = new WebSocket('ws://192.168.1.17:8080');
 
 socket.binaryType = 'arraybuffer';
 
@@ -10,50 +10,54 @@ socket.onopen = function(e) {
 	socketIsReady = true;
 };
 
-
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext('2d');
 
-let x = 0;
-let y = 0;
+let scaledWidth = 100;
+let scaledHeight = 100;
+let bytesPerPixel = 4;
+let tempPixels = new Uint8ClampedArray(scaledWidth * scaledHeight * bytesPerPixel);
+
+let scalePixels = function(buff) {
+	// arr is 4 * width * height
+	let arr = new Uint8ClampedArray(buff);
+
+	let pixels = [];
+	for (let i = 0; i < arr.length; i+=4) {
+		pixels.push(arr.slice(i, i+4));
+	}
+	
+	let newPixels2D = new Array(scaledWidth);
+	for (let i = 0; i < scaledWidth; i++) {
+		newPixels2D[i] = new Array(scaledHeight);
+	}
+	for (let i = 0; i < pixels.length; i++) {	
+		startX = 10 * Math.floor(i / 10);
+		startY = 10 * (i % 10);
+		for (let x = startX; x < startX + 10; x++) {
+			for (let y = startY; y < startY + 10; y++) {
+				newPixels2D[x][y] = pixels[i];
+			}
+		}
+	}				
+	
+	let newPixelsIndex = 0;
+	for (let i = 0; i < newPixels2D.length; i++) {
+		for (let j = 0; j < newPixels2D[0].length; j++) {
+			tempPixels[newPixelsIndex] = newPixels2D[i][j][0];
+			tempPixels[newPixelsIndex + 1] = newPixels2D[i][j][1];
+			tempPixels[newPixelsIndex + 2] = newPixels2D[i][j][2];
+			tempPixels[newPixelsIndex + 3] = newPixels2D[i][j][3];
+			newPixelsIndex+=4;
+		}
+	}
+
+	return tempPixels;
+};
 
 socket.onmessage = function(msg) {
-	let newPixels = new Uint8ClampedArray(msg.data);
-	let imageData = new ImageData(newPixels, 240, 135);
-  ctx.clearRect(0, 0, 240, 135);
-  ctx.putImageData(imageData, x++, y++);
+	let newPixels = scalePixels(msg.data);
+	let imageData = new ImageData(newPixels, 100, 100);
+  ctx.clearRect(0, 0, 100, 100);
+  ctx.putImageData(imageData, 0, 0);
 };
-
-const paintCanvas = function(ctx) {
-	// says pixels plz
-	if(socketIsReady) {
-		socket.send("p");
-	}
-	//let pixels = new Uint8ClampedArray(4 * 320 * 640);
-	//let pixelIndex = 0;
-
-	//let pixelRandom = Math.floor(Math.random() * 256);
-
-	//for(let y = 0; y < 320; y++) {
-	//	for(let x = 0; x < 640; x++) {
-	//	  pixels[pixelIndex++] = pixelRandom;
-	//	  pixels[pixelIndex++] = pixelRandom;
-	//		pixels[pixelIndex++] = 137;//pixelRandom;
-	//		pixels[pixelIndex++] = 255;
-	//	}
-	//}
-
-	//let justTheBuffer = pixels.buffer;
-
-	//let newPixels = new Uint8ClampedArray(justTheBuffer);
-
-	//let imageData = new ImageData(newPixels, 320, 640);
-
-	ctx.putImageData(imageData, 0, 0);
-};
-//const onAnimate = function(timestamp) {
-	//paintCanvas(ctx);
-	//window.requestAnimationFrame(onAnimate);
-//};
-
-//window.requestAnimationFrame(onAnimate);
