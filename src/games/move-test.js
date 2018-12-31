ac1d9b34bfdfe7bfdcdc5c28fcf0f5161e95618e
Joseph Garcia <joseph@unearthlabs.com>
2018-12-31 10:25:47 -0800

    center mover initially

--- src/games/move-test.js
+++ src/games/move-test.js
@@ -7,7 +7,7 @@ class MoveTest {
             {'x': 0, 'y': 0}, {'x': 1, 'y': 1});
 
         this.mover = gameNode(randomColor(), this.handleLayerClick,
-            {'x': .5, 'y': .5}, {'x': .1, 'y': .1});
+            {'x': .45, 'y': .45}, {'x': .1, 'y': .1});
 
         this.base.addChild(this.mover);
     }
