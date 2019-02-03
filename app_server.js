c2951d352298ed20fc0efe581ea0a9d2061849c8
Joseph Garcia <prosif@gmail.com>
2019-02-02 21:21:48 -0800

    redirect https

--- app_server.js
+++ app_server.js
@@ -1,10 +1,23 @@
 const fs = require('fs');
 const express = require('express');
 const https = require('https');
+const http = require('http');
 const app = express();
 
 const privateKey = fs.readFileSync('ssl/localhost.key');
 const certificate = fs.readFileSync('ssl/localhost.crt');
+
+const ensureSecure = (req, res, next) => {
+    if (req.secure) {
+        return next();
+    }
+
+    res.redirect('https://' + req.hostname + req.url);
+}
+
+app.all('*', ensureSecure);
+
+http.createServer(app).listen(80);
 
 https.createServer({
     key: privateKey,
