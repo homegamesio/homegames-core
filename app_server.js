e1c864acade2f79cfac197f71fbe3e5521501691
Joseph Garcia <prosif@gmail.com>
2019-02-16 12:04:22 -0800

    remove express, uuid

--- app_server.js
+++ app_server.js
@@ -1,8 +1,38 @@
-const express = require("express");
 const http = require("http");
+const fs = require('fs');
 
-const app = express();
+const index = fs.readFileSync('web/index.html');
+const app = fs.readFileSync('web/app.js');
+const style = fs.readFileSync('web/app.css');
 
-http.createServer(app).listen(80);
+http.createServer((req, res) => {
+    const path = req.url;
+    let contentType, payload;
 
-app.use(express.static("web"));
+    switch(path) {
+        case '/': {
+            contentType = 'text/html';
+            payload = index;
+            break;
+        }
+        case '/app.js': {
+            contentType = 'text/javascript';
+            payload = app;
+            break;
+        }
+        case '/app.css': {
+            contentType = 'text/css';
+            payload = style;
+            break;
+        }
+        default: {
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', contentType);
+    res.end(payload);
+}).listen(80);
