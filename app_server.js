1ad36feaadfd081c584ffda14d2b9a0bd9963671
Joseph Garcia <prosif@gmail.com>
2019-07-14 15:05:25 -0700

    hot swap app.js

--- app_server.js
+++ app_server.js
@@ -1,26 +1,25 @@
 const http = require("http");
 const fs = require('fs');
 
-const index = fs.readFileSync('web/index.html');
-const app = fs.readFileSync('web/app.js');
-const style = fs.readFileSync('web/app.css');
-
 http.createServer((req, res) => {
     const path = req.url;
     let contentType, payload;
 
     switch(path) {
         case '/': {
+            const index = fs.readFileSync('web/index.html');
             contentType = 'text/html';
             payload = index;
             break;
         }
         case '/app.js': {
+            const app = fs.readFileSync('web/app.js');
             contentType = 'text/javascript';
             payload = app;
             break;
         }
         case '/app.css': {
+            const style = fs.readFileSync('web/app.css');
             contentType = 'text/css';
             payload = style;
             break;
