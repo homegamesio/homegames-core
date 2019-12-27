9949376f987eb3ca765098def1b2bdf460f43fab
Joseph Garcia <prosif@gmail.com>
2019-12-27 13:43:10 -0700

    charades wip

--- src/games/charades.js
+++ src/games/charades.js
@@ -12,12 +12,32 @@ class Charades {
     }
 
     constructor() {
-        this.base = gameNode(Colors.BLUE, (player) => {
+        this.base = gameNode(Colors.WHITE, (player) => {
             player.receiveUpdate([5, 70, 0]);
         }, {"x": 0, "y": 0}, {"x": 100, "y": 100}, {"text": "ayy lmao", x: 50, y: 5});
     }
 
-    handleNewPlayer() {
+    handleNewPlayer(player) {
+        const playerInfoNode = gameNode(
+            Colors.WHITE,
+            (player) => {
+
+            },
+            {
+                x: 10,
+                y: Object.values(this.players).length * 8
+            },
+            {
+                x: 10,
+                y: 8
+            },
+            {
+                text: player.name,
+                x: 10,
+                y: Object.values(this.players).length * 8
+            }
+        );
+        this.base.addChild(playerInfoNode);
     }
 
     handlePlayerDisconnect() {
