49b279faba238f80950b96b808a85e6c7406e84d
Joseph Garcia <prosif@gmail.com>
2019-12-28 22:50:46 -0700

    make perf test more useful

--- src/games/perf-test.js
+++ src/games/perf-test.js
@@ -15,17 +15,34 @@ class PerfTest {
         this.base = gameNode(Colors.WHITE, (player) => {
         }, {"x": 0, "y": 0}, {"x": 100, "y": 100});
 
-        this.guy1 = gameNode(Colors.RED, null, {x: 30, y: 0}, {x: 10, y: 10});
-        this.guy2 = gameNode(Colors.GREEN, null, {x: 50, y: 0}, {x: 10, y: 10});
-        this.guy3 = gameNode(Colors.BLUE, null, {x: 70, y: 0}, {x: 10, y: 10});
-        
-        this.base.addChild(this.guy1);
-        this.base.addChild(this.guy2);
-        this.base.addChild(this.guy3);
+        let xCounter = 0;
+        let yCounter = 0;
 
-        this.guy1Interval = setInterval(this.moveGuy1.bind(this), 20);
-        this.guy2Interval = setInterval(this.moveGuy2.bind(this), 40);
-        this.guy3Interval = setInterval(this.moveGuy3.bind(this), 80);
+        const filler = setInterval(() => {
+            let dot = gameNode(Colors.randomColor(), null, {x: xCounter, y: yCounter}, {x: 1, y: 1});
+            this.base.addChild(dot);
+            xCounter += 1;
+            if (xCounter >= 100) {
+                xCounter = 0;
+                yCounter++;
+            }
+
+            if (yCounter == 100 && xCounter == 100) {
+                clearInterval(filler);
+            }
+
+        }, 2);
+        //this.guy1 = gameNode(Colors.RED, null, {x: 30, y: 0}, {x: 10, y: 10});
+        //this.guy2 = gameNode(Colors.GREEN, null, {x: 50, y: 0}, {x: 10, y: 10});
+        //this.guy3 = gameNode(Colors.BLUE, null, {x: 70, y: 0}, {x: 10, y: 10});
+        
+        //this.base.addChild(this.guy1);
+        //this.base.addChild(this.guy2);
+        //this.base.addChild(this.guy3);
+
+        //this.guy1Interval = setInterval(this.moveGuy1.bind(this), 20);
+        //this.guy2Interval = setInterval(this.moveGuy2.bind(this), 40);
+        //this.guy3Interval = setInterval(this.moveGuy3.bind(this), 80);
     }
 
     moveGuy1() {
