a20e36a53f1c8de521d3781a69cb54a5d0f6e1ce
Joseph Garcia <prosif@gmail.com>
2020-02-08 20:13:14 -0800

    wip

--- game_server.js
+++ game_server.js
@@ -3,10 +3,12 @@ const PerfTest = require('./src/games/perf-test');
 const GameSession = require("./src/GameSession");
 const { socketServer } = require('./src/util/socket');
 const config = require('./config');
+const GameThing = require('./src/games/game-thing');
 
-const dashboard = new HomegamesDashboard();
+//const dashboard = new HomegamesDashboard();
+const game = new GameThing();
 
-const session = new GameSession(dashboard);
+const session = new GameSession(game);
 
 session.initialize(() => {
     socketServer(session, config.GAME_SERVER_HOME_PORT);
--- src/games/game-thing.js
+++ src/games/game-thing.js
@@ -1,6 +1,7 @@
 const { GameNode, Colors } = require('squishjs');
 const Asset = require('../common/Asset');
 const Game = require('./Game');
+const { checkCollisions } = require('../common/util');
 
 class GameThing extends Game {
     static metadata() {
@@ -18,30 +19,56 @@ class GameThing extends Game {
         super();
         this.base = GameNode(Colors.BLUE, (player) => {
 
-        }, {'x': 0, 'y': 0}, {'x': 100, 'y': 100}, {'text': 'ayy lmao', x: 50, y: 5});
+        }, {'x': 0, 'y': 0}, {'x': 100, 'y': 100});
         
         this.misterSticksFrames = {
-            'initial': 'https://homegamesio.s3-us-west-1.amazonaws.com/sprites/test1.png',
-            'kickin': 'https://homegamesio.s3-us-west-1.amazonaws.com/sprites/test2.png'
+            idleLeft: 'https://homegamesio.s3-us-west-1.amazonaws.com/sprites/test1.png',
+            kickRight: 'https://homegamesio.s3-us-west-1.amazonaws.com/sprites/test2.png'
+        };
+
+        this.npcFrames = {
+            walker: 'https://homegamesio.s3-us-west-1.amazonaws.com/sprites/walkerthing.png'
         };
         
-        this.assets = {
-            'mister_sticks_initial': new Asset('url', {
-                'location': this.misterSticksFrames['initial'],
+        const allAssets = Object.assign(this.misterSticksFrames, this.npcFrames);
+        this.assets = {};
+        for (let key in allAssets) {
+            this.assets[key] = new Asset('url', {
+                'location': allAssets[key],
                 'type': 'image'
-            }),
-            'mister_sticks_kickin': new Asset('url', {
-                'location': this.misterSticksFrames['kickin'],
-                'type': 'image'
-            })
-        };
-
+            });
+        }
 
         this.misterSticks = GameNode(null, (player) => {
-            console.log("i'm mister sticks");
-        }, {x: 40, y: 40}, {x: 10, y: 10}, null, {'mister_sticks_initial': {pos: {x: 40, y: 40}, size: {x: 15, y: 15}}});
-        console.log(this.misterSticks);
+        }, {x: 40, y: 40}, {x: 10, y: 10}, null, {idleLeft: {pos: {x: 40, y: 40}, size: {x: 15, y: 15}}});
+
+        this.walkerGuy = GameNode(null, (player) => {
+
+        }, {x: 5, y: 5}, {x: 10, y: 10}, null, {walker: {pos: {x: 5, y: 5}, size: {x: 10, y: 10}}});
+        this.base.addChild(this.walkerGuy);
         this.base.addChild(this.misterSticks);
+    }
+
+    tick() {
+        const guyCollisions = checkCollisions(this.base, this.misterSticks, (node) => node.id !== this.misterSticks.id && node.id !== this.base.id);
+        if (guyCollisions.length > 0) {
+            if (this.misterSticks.isKickin) {
+                const currentWalkerAsset = Object.values(this.walkerGuy.asset)[0];
+                const newPos = this.walkerGuy.pos;
+                
+                currentWalkerAsset.pos.x = (currentWalkerAsset.pos.x + 40) % 100;
+                newPos.x = (newPos.x + 40) % 100;
+
+                this.walkerGuy.pos = newPos;
+//                this.walkerGuy.asset = currentWalkerAsset;
+            } else {
+                Object.values(this.misterSticks.asset)[0].pos = {
+                    x: 40,
+                    y: 40
+                };
+                this.misterSticks.pos = {x: 40, y: 40};
+            }
+        }
     }
 
     handleNewPlayer(player) {
@@ -51,34 +78,56 @@ class GameThing extends Game {
     }
 
     handleKeyUp(player, key) {
-        const newAsset = {
-            'mister_sticks_initial': {
-                pos: {
-                    x: 40, 
-                    y: 40
-                },
-                size: {
-                    x: 15, 
-                    y: 15
+        if (key === ' ') {
+            const currentAsset = Object.values(this.misterSticks.asset)[0];
+            const newAsset = {
+                idleLeft: {
+                    pos: currentAsset.pos,
+                    size: currentAsset.size
                 }
-            }
-        };
-        this.misterSticks.asset = newAsset;
+            };
+            this.misterSticks.isKickin = false;
+            this.misterSticks.asset = newAsset;
+        }
     }
 
     handleKeyDown(player, key) {
-        const newAsset = {
-            'mister_sticks_kickin': {
-                pos: {
-                    x: 40, 
-                    y: 40
-                },
-                size: {
-                    x: 15, 
-                    y: 15
+        let moveDir;
+        let moveDis;
+
+        if (key == 'd' || key == 'ArrowRight') {
+            moveDir = 'x';
+            moveDis = .1;
+        } else if (key == 'w' || key == 'ArrowUp') {
+            moveDir = 'y';
+            moveDis = -.1;
+        } else if (key == 'a' || key == 'ArrowLeft') {
+            moveDir = 'x';
+            moveDis = -.1;
+        } else if (key == 's' || key == 'ArrowDown') {
+            moveDir = 'y';
+            moveDis = .1;
+        } else if (key == ' ') {
+            const currentAsset = Object.values(this.misterSticks.asset)[0];
+            const newAsset = {
+                kickRight: {
+                    pos: currentAsset.pos,
+                    size: currentAsset.size
                 }
-            }
-        };
+            };
+            this.misterSticks.isKickin = true;
+            this.misterSticks.asset = newAsset;
+            return;
+        } else {
+            return;
+        }
+
+        let newPos = this.misterSticks.pos;
+        newPos[moveDir] += moveDis;
+        let newAsset = this.misterSticks.asset;
+        Object.values(newAsset)[0].pos[moveDir] += moveDis;
+
+        this.misterSticks.pos = newPos;
         this.misterSticks.asset = newAsset;
     }
 
