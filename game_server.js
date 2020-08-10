56110a114c118518b3ad8a828b89842b66f719c6
Joseph Garcia <prosif@gmail.com>
2020-08-09 20:15:48 -0700

    wip

--- game_server.js
+++ game_server.js
@@ -3,9 +3,12 @@ const GameSession = require("./src/GameSession");
 const { socketServer } = require('./src/util/socket');
 const config = require('./config');
 const Homenames = require('./src/Homenames');
-const clickCity = require('./src/games/click-city');
+const ClickCity = require('./src/games/click-city');
+const Game42 = require('./src/games/game-42');
 
-const dashboard = new clickCity();//HomegamesDashboard();
+//const dashboard = new clickCity();//HomegamesDashboard();
+//const dashboard = new HomegamesDashboard();
+const dashboard = new Game42();
 
 const session = new GameSession(dashboard);
 
