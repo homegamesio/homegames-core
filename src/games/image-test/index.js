be3e3b8be30c44533b300f414c33f399c8fe7c5b
Joseph Garcia <prosif@gmail.com>
2021-01-10 18:40:13 -0700

    wip

--- src/games/image-test/index.js
+++ src/games/image-test/index.js
@@ -24,60 +24,52 @@ class ImageTest extends Game {
         const defaultImageSize = {x: 10 * (aspectRatio.y/aspectRatio.x), y: 10};// * (aspectRatio.y / aspectRatio.x)};
         const defaultImagePos = {x: 45, y: 40};
 
-        this.base = new GameNode.Shape(COLORS.WHITE, 
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [0, 0],
-                    [100, 0],
-                    [100, 100],
-                    [0, 100],
-                    [0, 0]
-                ],
-                fill: COLORS.WHITE
-            },
-            null,
-            (player, x, y) => {
+        this.base = new GameNode.Shape({ 
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [0, 0],
+                [100, 0],
+                [100, 100],
+                [0, 100],
+                [0, 0]
+            ],
+            fill: COLORS.WHITE,
+            onClick: (player, x, y) => {
                 const newAsset = this.imageNode.node.asset;
                 newAsset.image.pos = {x, y};
                 this.imageNode.node.asset = newAsset;
             }
-        );
+        });
         
-        this.imageNode = new GameNode.Asset(
-            null,
-            [
+        this.imageNode = new GameNode.Asset({
+            coordinates2d: [
                 [defaultImagePos.x, defaultImagePos.y],
                 [defaultImagePos.x + defaultImageSize.x, defaultImagePos.y],
                 [defaultImagePos.x + defaultImageSize.x, defaultImagePos.y + defaultImageSize.y],
                 [defaultImagePos.x, defaultImagePos.y + defaultImagePos.y],
                 [defaultImagePos.x, defaultImagePos.y]
             ],
-            {
+            assetInfo: {
                 'image': {
                     'pos': Object.assign({}, defaultImagePos),
                     'size': Object.assign({}, defaultImageSize)
                 }
             }
-        );
+        });
 
         this.base.addChild(this.imageNode);
 
-        this.decreaseWidthButton = new GameNode.Shape(
-            COLORS.RED,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [29.5, 2],
-                    [29.5 + 6, 2],
-                    [29.5 + 6, 2 + (6 * 16 / 9)],
-                    [29.5, 2 + (6 * 16 / 9)],
-                    [29.5, 2],
-                ],
-                fill: COLORS.RED
-            },
-            null,
-            (player, x, y) => {
+        this.decreaseWidthButton = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [29.5, 2],
+                [29.5 + 6, 2],
+                [29.5 + 6, 2 + (6 * 16 / 9)],
+                [29.5, 2 + (6 * 16 / 9)],
+                [29.5, 2],
+            ],
+            fill: COLORS.RED,
+            onClick: (player, x, y) => {
                 if (this.imageNode.node.asset.image.size.x < 1) {
                     return;
                 }
@@ -85,23 +77,19 @@ class ImageTest extends Game {
                 newAsset.image.size.x -= 1;
                 this.imageNode.node.asset = newAsset;
             }
-        );
+        });
         
-        this.increaseWidthButton = new GameNode.Shape(
-            COLORS.GREEN,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [49.5, 2],
-                    [49.5 + 6, 2],
-                    [49.5 + 6, 2 + (6 * 16 / 9)],
-                    [49.5, 2 + (6 * 16 / 9)],
-                    [49.5, 2],
-                ],
-                fill: COLORS.GREEN
-            },
-            null,
-            (player, x, y) => {
+        this.increaseWidthButton = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [49.5, 2],
+                [49.5 + 6, 2],
+                [49.5 + 6, 2 + (6 * 16 / 9)],
+                [49.5, 2 + (6 * 16 / 9)],
+                [49.5, 2],
+            ],
+            fill: COLORS.GREEN,
+            onClick: (player, x, y) => {
                 if (this.imageNode.node.asset.image.size.x > 80) {
                     return;
                 }
@@ -109,23 +97,19 @@ class ImageTest extends Game {
                 newAsset.image.size.x += 1;
                 this.imageNode.node.asset = newAsset;
             }
-        );
+        });
 
-        this.decreaseHeightButton = new GameNode.Shape(
-            COLORS.RED,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [29.5, 22],
-                    [29.5 + 6, 22],
-                    [29.5 + 6, 22 + (6 * 16 / 9)],
-                    [29.5, 22 + (6 * 16 / 9)],
-                    [29.5, 22],
-                ],
-                fill: COLORS.RED
-            },
-            null,
-            (player, x, y) => {
+        this.decreaseHeightButton = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [29.5, 22],
+                [29.5 + 6, 22],
+                [29.5 + 6, 22 + (6 * 16 / 9)],
+                [29.5, 22 + (6 * 16 / 9)],
+                [29.5, 22],
+            ],
+            fill: COLORS.RED,
+            onClick: (player, x, y) => {
                 if (this.imageNode.node.asset.image.size.y < 1) {
                     return;
                 }
@@ -133,23 +117,19 @@ class ImageTest extends Game {
                 newAsset.image.size.y -= 1;
                 this.imageNode.node.asset = newAsset;
             }
-        );
+        });
         
-        this.increaseHeightButton = new GameNode.Shape(
-            COLORS.GREEN,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [49.5, 22],
-                    [49.5 + 6, 22],
-                    [49.5 + 6, 22 + (6 * 16 / 9)],
-                    [49.5, 22 + (6 * 16 / 9)],
-                    [49.5, 22],
-                ],
-                fill: COLORS.GREEN
-            },
-            null,
-            (player, x, y) => {
+        this.increaseHeightButton = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [49.5, 22],
+                [49.5 + 6, 22],
+                [49.5 + 6, 22 + (6 * 16 / 9)],
+                [49.5, 22 + (6 * 16 / 9)],
+                [49.5, 22],
+            ],
+            fill: COLORS.GREEN,
+            onClick: (player, x, y) => {
                 if (this.imageNode.node.asset.image.size.y > 80) {
                     return;
                 }
@@ -157,23 +137,19 @@ class ImageTest extends Game {
                 newAsset.image.size.y += 1;
                 this.imageNode.node.asset = newAsset;
             }
-        );
+        });
         
-        this.resetButton = new GameNode.Shape(
-            COLORS.WHITE,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [79.5, 22],
-                    [79.5 + 6, 22],
-                    [79.5 + 6, 22 + (6 * 16 / 9)],
-                    [79.5, 22 + (6 * 16 / 9)],
-                    [79.5, 22],
-                ],
-                fill: COLORS.HG_YELLOW
-            },
-            null,
-            (player, x, y) => {
+        this.resetButton = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [79.5, 22],
+                [79.5 + 6, 22],
+                [79.5 + 6, 22 + (6 * 16 / 9)],
+                [79.5, 22 + (6 * 16 / 9)],
+                [79.5, 22],
+            ],
+            fill: COLORS.HG_YELLOW,
+            onClick: (player, x, y) => {
                 this.imageNode.node.coordinates2d = [
                     [defaultImagePos.x, defaultImagePos.y],
                     [defaultImagePos.x + defaultImageSize.x, defaultImagePos.y],
@@ -189,7 +165,7 @@ class ImageTest extends Game {
                     }
                 };              
             }
-        );
+        });
 
         this.base.addChild(this.decreaseWidthButton);
         this.base.addChild(this.increaseWidthButton);
--- src/games/layer-test/index.js
+++ src/games/layer-test/index.js
@@ -15,42 +15,35 @@ class LayerTest extends Game {
     constructor() {
         super();
         const baseColor = Colors.randomColor();
-        this.base = new GameNode.Shape(
-            baseColor,
-            Shapes.POLYGON,
-            {
-                coordinates2d: [
-                    [0, 0],
-                    [100, 0],
-                    [100, 100],
-                    [0, 100],
-                    [0, 0]
-                ],
-                fill: baseColor
-            },
-            null,
-            this.handleLayerClick);
+        this.base = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            coordinates2d: [
+                [0, 0],
+                [100, 0],
+                [100, 100],
+                [0, 100],
+                [0, 0]
+            ],
+            fill: baseColor,
+            onClick: this.handleLayerClick
+        });
 
         const increment = 1;
         let prev = this.base;
         for (let i = increment; i < 50; i+= 2 * increment) {
             const childColor = Colors.randomColor();
-            const child = new GameNode.Shape(
-                childColor,
-                Shapes.POLYGON,
-                {
-                    coordinates2d: [
-                        [i, i],
-                        [i + 100 - (2 * i), i],
-                        [i + 100 - (2 * i), i + 100 - (2 * i)],
-                        [i, i + 100 - (2 * i)],
-                        [i, i]
-                    ],
-                    fill: childColor
-                },
-                null,
-                this.handleLayerClick
-            );
+            const child = new GameNode.Shape({
+                shapeType: Shapes.POLYGON,
+                coordinates2d: [
+                    [i, i],
+                    [i + 100 - (2 * i), i],
+                    [i + 100 - (2 * i), i + 100 - (2 * i)],
+                    [i, i + 100 - (2 * i)],
+                    [i, i]
+                ],
+                fill: childColor,
+                onClick: this.handleLayerClick
+            });
             prev.addChild(child);
             prev = child;
         }
