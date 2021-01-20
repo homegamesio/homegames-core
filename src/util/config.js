356708b86fa21fe0ac2c1f02ef576444b0c56e97
Joseph Garcia <prosif@gmail.com>
2021-01-19 21:02:07 -0700

    actually fix config I think

--- src/util/config.js
+++ src/util/config.js
@@ -5,31 +5,27 @@ const baseDir = path.dirname(require.main.filename);
 
 const getConfigValue = (key, _default = undefined) => {
     let envValue = process.env[key] && `${process.env[key]}`;
-    console.log(process.env);
-    console.log("helllllo " + key);
-    console.log(envValue);
     if (envValue !== undefined) {
         if (envValue === 'true') {
-            console.log('hhhhhhhhh brotjher');
-            //envValue = true;
+            envValue = true;
         } else if (envValue === 'false') {
-            //envValue = false;
+            envValue = false;
         }
         console.log(`Using environment value: ${envValue} for key: ${key}`);
         return envValue;
     }
     try {
-        const _config = require(`${baseDir}/config`);
-        console.log(`Using config at ${baseDir}/config`);
+        const _config = require(`${process.cwd()}/config`);
+        console.log(`Using config at ${process.cwd()}/config`);
         if (!_config[key]) {
-            throw new Error(`No value for ${key} found in ${baseDir}/config`);
+            throw new Error(`No value for ${key} found in ${process.cwd()}/config`);
         }
         return _config[key];
     } catch(err) {
         if (_default === undefined) {
             throw new Error(`No config value found for ${key}`);
         } else {
-            console.log(`Could not find config at ${baseDir}/config for key ${key}. Using default: ${_default}`);
+            console.log(`Could not find config at ${process.cwd()}/config for key ${key}. Using default: ${_default}`);
             return _default;
         }
     }
