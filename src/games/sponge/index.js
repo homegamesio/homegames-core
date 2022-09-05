24707b021ec86fc8d1b01939e03eac7c92b61fbb
Joseph Garcia <prosif@gmail.com>
2022-09-05 10:39:15 -0700

    horrific

--- src/games/sponge/index.js
+++ src/games/sponge/index.js
@@ -152,8 +152,8 @@ class Sponge extends Game {
             randXVel = 1;
         }
 
-        randXVel = 1;
-        randYVel = .5;
+        // randXVel = 1;
+        // randYVel = -1;
         const ballPath = Physics.getPath(ballX, ballY, randXVel, randYVel, 100 - BALL_SIZE, 100 - BALL_SIZE);
 
         this.moveBall(ballPath);
@@ -196,6 +196,8 @@ class Sponge extends Game {
             const bounce = (dir) => {
                 clearInterval(interval);
                 console.log('bouncing ' + dir);
+                console.log('uhhhh ' + coordIndex);
+                console.log(path);
                 // console.log(path);
                 const secondToLast = path[coordIndex - 2];//path.length - 2];
                 const mostRecent = path[coordIndex - 1];//path.length - 1];
@@ -203,29 +205,192 @@ class Sponge extends Game {
                 const xDiff = mostRecent[0] - secondToLast[0];
                 const yDiff = mostRecent[1] - secondToLast[1];
 
-                if (dir && dir === 'left') {
-                    this.ball.node.coordinates2d = ShapeUtils.rectangle(5.25, curBallY, BALL_SIZE, BALL_SIZE);
-                } else if (dir && dir === 'right') {
-                    this.ball.node.coordinates2d = ShapeUtils.rectangle(89.75, curBallY, BALL_SIZE, BALL_SIZE);
-                }
+                // if (dir && dir === 'left') {
+                //     this.ball.node.coordinates2d = ShapeUtils.rectangle(5.25, curBallY, BALL_SIZE, BALL_SIZE);
+                // } else if (dir && dir === 'right') {
+                //     this.ball.node.coordinates2d = ShapeUtils.rectangle(89.75, curBallY, BALL_SIZE, BALL_SIZE);
+                // }
 
                 console.log("what was x diff y diff " + xDiff + ", " + yDiff);
                 console.log('final point');
                 console.log(finalPoint);
+                console.log('the fuck 1 fsdfsdf ' + (finalPoint[1] + Math.abs(yDiff) + BALL_SIZE));
+                console.log('the fuck 2 fsdfsdf ' + (finalPoint[0] + Math.abs(xDiff) + BALL_SIZE));
+                console.log('the fuck 3 fsdfsdf ' + (finalPoint[1] - Math.abs(yDiff) - BALL_SIZE));
+                console.log('the fuck 4 fsdfsdf ' + (finalPoint[0] - Math.abs(xDiff) - BALL_SIZE));
+                console.log('literally what 1' + ((finalPoint[0] + Math.abs(xDiff) + BALL_SIZE >= 100)));
+                console.log('literally what 2' + (finalPoint[0] - Math.abs(xDiff) - BALL_SIZE <= 0));
+                console.log('literally what 3' + (finalPoint[1] + Math.abs(yDiff) + BALL_SIZE >= 100));
+                console.log('literally what 4' + (finalPoint[1] - Math.abs(yDiff) - BALL_SIZE <= 0));
 
-                // invert x if left or right bounce
-                const newX = finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                if ((finalPoint[0] + Math.abs(xDiff) + BALL_SIZE >= 100 || finalPoint[0] - Math.abs(xDiff) <= 0) && 
+                    (finalPoint[1] + Math.abs(yDiff) + BALL_SIZE >= 100 || finalPoint[1] - Math.abs(yDiff) <= 0)) {
 
-                // invert y if top or bottom bounce
-                const newY = finalPoint[1] - Math.abs(yDiff) < 0 ? Math.abs(yDiff) : Math.abs(yDiff) * -1;//finalPoint[1] <= BALL_SIZE || mostRecent[1] >= (100 - BALL_SIZE - 1) ? -1 * yDiff : yDiff;
+                  console.log('issa corner');//right down. need right up');
+                  const newX = -1 * xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                  const newY = -1 * yDiff;
+
+                  console.log('new x y ' + (newX) + ', ' + newY)
 
                 const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
                 this.playSound(dir);
                 console.log("bouncing with new x y " + newX + ", " + newY);
-                this.moveBall(newPath);        
+                this.moveBall(newPath);
+                } else {
+                    console.log('not corner');
+                    // right down
+                    if (xDiff > 0 && yDiff > 0) {
+                        if (finalPoint[1] + yDiff + BALL_SIZE >= 100) {
+                            if (finalPoint[0] + xDiff + BALL_SIZE >= 100) {
+                                console.log('corner?');
+
+                                  console.log('right down. need right up');
+                                  const newX = -1 * Math.abs(xDiff);// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                  const newY = -1 * Math.abs(yDiff);
+
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.playSound(dir);
+                                console.log("bouncing with new x y " + newX + ", " + newY);
+                                this.moveBall(newPath);
+                            } else {
+                                  console.log('right down. need right up');
+                                  const newX = xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                  const newY = -1 * Math.abs(yDiff);
+
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.playSound(dir);
+                                console.log("bouncing with new x y " + newX + ", " + newY);
+                                this.moveBall(newPath);
+                            }
+                        } else {
+                            console.log('right down. need left down');
+                            // invert x if left or right bounce
+                            const newX = xDiff * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+
+                            // invert y if top or bottom bounce
+                            const newY = yDiff;//finalPoint[1] - Math.abs(yDiff) < 0 ? Math.abs(yDiff) : Math.abs(yDiff) * -1;//finalPoint[1] <= BALL_SIZE || mostRecent[1] >= (100 - BALL_SIZE - 1) ? -1 * yDiff : yDiff;
+
+                            const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                            this.playSound(dir);
+                            console.log("bouncing with new x y " + newX + ", " + newY);
+                            this.moveBall(newPath);
+                        }
+                    } else if (xDiff < 0 && yDiff > 0) {
+                        console.log('what the fuck');
+                        console.log(finalPoint)
+                        if (finalPoint[0] - Math.abs(xDiff) - BALL_SIZE < 0) {
+
+                            if (finalPoint[1] + Math.abs(yDiff) + BALL_SIZE > 100) {
+                                console.log('left down. need left up');
+
+                                const newX = xDiff;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = -1 * yDiff;
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                console.log('path is now');
+                                console.log(newPath)
+                                this.moveBall(newPath);
+                            } else {
+                                console.log('left down. need right down')
+                                const newX = -1 * xDiff;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = yDiff;
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            }
+                            // if (yDiff > 0) {
+
+                            // } else {
+                            //     console.log('left down. need left up');
+                            // }
+                        
+                        } else {
+                            if (finalPoint[1] + yDiff > (100 - BALL_SIZE)) {
+                                console.log('left down. need left up')
+                                const newX = xDiff;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = -1 * yDiff;
+                                console.log('wat ' + newX + ', ' + newY);
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                console.log(newPath)
+                                this.moveBall(newPath);
+                            } else {
+                                console.log('left down. need right down')
+                                const newX = xDiff;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = -1 * yDiff;
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            };
+                        }
+                        // const newX = finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+
+                        // const newY = yDiff * -1;//finalPoint[1] - Math.abs(yDiff) < 0 ? Math.abs(yDiff) : Math.abs(yDiff) * -1;//finalPoint[1] <= BALL_SIZE || mostRecent[1] >= (100 - BALL_SIZE - 1) ? -1 * yDiff : yDiff;
+
+                        // const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                        // this.playSound(dir);
+                        // console.log("bouncing with new x y " + newX + ", " + newY);
+                        // this.moveBall(newPath);
+                        // invert x if left or right bounce
+                        // const newX = xDiff * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+
+                        // // invert y if top or bottom bounce
+                        // const newY = yDiff;//finalPoint[1] - Math.abs(yDiff) < 0 ? Math.abs(yDiff) : Math.abs(yDiff) * -1;//finalPoint[1] <= BALL_SIZE || mostRecent[1] >= (100 - BALL_SIZE - 1) ? -1 * yDiff : yDiff;
+
+                        // const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                        // this.playSound(dir);
+                        // console.log("bouncing with new x y " + newX + ", " + newY);
+                        // this.moveBall(newPath);
+                    } else {
+                        console.log('x diff y diff ' + xDiff + ", " + yDiff);
+                        if (xDiff < 0 && yDiff < 0) {
+                            console.log('left up, need right up or left down');
+                            if (finalPoint[0] - Math.abs(xDiff) < 0) {
+                                console.log('right up');
+
+                                const newX = Math.abs(xDiff);//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = -1 * Math.abs(yDiff);
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            } else {
+                                console.log('left down');
+
+                                const newX = -1 * Math.abs(xDiff);//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = Math.abs(yDiff);
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            }
+                        } else if (xDiff > 0 && yDiff < 0) {
+                            console.log('right up. need left up or right down');
+                            console.log('huhhh ' + xDiff + ", " + yDiff);
+                            console.log(finalPoint)
+                            if (finalPoint[0] + Math.abs(xDiff) + BALL_SIZE >= 100) {
+                                console.log('left up');
+
+                                const newX = -1 * Math.abs(xDiff);//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = -1 * Math.abs(yDiff);
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            } else {
+                                console.log('right down');
+                                const newX = Math.abs(xDiff);//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : xDiff;// * -1;//finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+                                const newY = Math.abs(yDiff);
+                                const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                                this.moveBall(newPath);
+                            }
+
+                        }
+                        // invert x if left or right bounce
+                        // const newX = finalPoint[0] - Math.abs(xDiff) < 0 ? Math.abs(xDiff) : Math.abs(xDiff) * -1;//dir && dir === 'left' || dir === 'right' ? -1 * xDiff : xDiff;
+
+                        // // invert y if top or bottom bounce
+                        // const newY = finalPoint[1] - Math.abs(yDiff) < 0 ? Math.abs(yDiff) : Math.abs(yDiff) * -1;//finalPoint[1] <= BALL_SIZE || mostRecent[1] >= (100 - BALL_SIZE - 1) ? -1 * yDiff : yDiff;
+
+                        // const newPath = Physics.getPath(this.ball.node.coordinates2d[0][0], this.ball.node.coordinates2d[0][1], newX, newY, 100 - BALL_SIZE, 100 - BALL_SIZE);
+                        // this.playSound(dir);
+                        // console.log("bouncing with nesadfasdfw x y " + newX + ", " + newY);
+                        // this.moveBall(newPath);        
+                    }
+                }
             };
  
-            if (coordIndex >= path.length) {
+            if (coordIndex == path.length) {
                 bounce();
             } else {
                //  if (curBallX <= BALL_SIZE) {
