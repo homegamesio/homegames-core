8d26a3b0eb8e93ad77858f3f4659e6f6b6729dec
Joseph Garcia <prosif@gmail.com>
2023-05-29 18:33:12 -0700

    nice

--- src/games/vegas-trail/index.js
+++ src/games/vegas-trail/index.js
@@ -2,6 +2,111 @@ const { Game, GameNode, Colors, Shapes, ShapeUtils, GeometryUtils } = require('s
 const { MapGame, Drive, Fight, Hunt, Stats } = require('./minigames/index.js');
 const COLORS = Colors.COLORS;
 
+const mapData = {
+    mapCoords: [
+       [92, 92],
+       [90, 85],
+       [85, 85],
+       [80, 80],
+       [75, 78],
+       [70, 78],
+       [70, 76],
+       [75, 72],
+       [75, 68],
+       [72, 66],
+       [71, 65],
+       [70, 64],
+       [69, 64],
+       [68, 64],
+       [67, 62],
+       [66, 57],
+       [66, 55],
+       [67, 54],
+       [68, 52],
+       [66, 48],
+       [66, 47],
+       [65, 45],
+       [63, 44],
+       [62, 43.5],
+       [60, 43],
+       [58, 42.5],
+       [56, 42.5],
+       [54, 44],
+       [52, 44],
+       [50, 44],
+       [47, 48],
+       [42, 48],
+       [38, 46],
+       [34, 45],
+       [32, 40],
+       [32, 36],
+       [31, 32],
+       [31, 26],
+       [30, 24],
+       [28, 22],
+       [26, 22],
+       [25, 21],
+       [23, 20.5],
+       [20, 20.5],
+       [16, 19],
+       [12, 19],
+       [11, 18],
+       [10, 14],
+       [9, 10],
+       [8, 8],
+       [8, 12],
+       [10, 18],
+       [11, 20],
+       [12, 21],
+       [16, 21],
+       [20, 22.5],
+       [23, 22.5],
+       [25, 23],
+       [26, 24],
+       [28, 24],
+       [30, 26],
+       [30, 28],
+       [30, 34],
+       [31, 38],
+       [32, 42],
+       [34, 47],
+       [38, 48],
+       [42, 50],
+       [47, 50],
+       [50, 46],
+       [52, 46],
+       [54, 46],
+       [56, 44.5],
+       [58, 44.5],
+       [60, 45],
+       [62, 45.5],
+       [63, 46],
+       [65, 47],
+       [66, 49],
+       [66, 50],
+       [67, 53],
+       [65, 54],
+       [65, 57],
+       [67, 64],
+       [68, 66],
+       [69, 66],
+       [70, 66],
+       [71, 67],
+       [72, 68],
+       [74, 70],
+       [72, 73],
+       [67, 78],
+       [70.5, 80],
+       [75, 81],
+       [80, 82],
+       [85, 87],
+       [92, 92]
+   ],
+   landmarks: [
+       
+   ]
+};
+
 const mapOptionNode = (onClick) => {
     return new GameNode.Shape({
         shapeType: Shapes.POLYGON,
@@ -60,8 +165,9 @@ class VegasTrail extends Game {
 
     constructor(initialState ={}) {
         super();
-
-        this.map = new MapGame();
+    
+        this.playerStates = {};
+        this.map = new MapGame(this, mapData);
         this.drive = new Drive();
         this.hunt = new Hunt();
         this.fight = new Fight();
@@ -109,6 +215,20 @@ class VegasTrail extends Game {
     }
 
     handleNewPlayer({ playerId }) {
+        const node = new GameNode.Shape({
+            shapeType: Shapes.POLYGON,
+            fill: Colors.COLORS.PURPLE,
+            coordinates2d: ShapeUtils.rectangle(mapData.mapCoords[0][0], mapData.mapCoords[0][1], 2, 2)
+        });
+
+        this.playerStates[playerId] = {
+            path: Object.assign(new Array(), mapData.mapCoords),
+            currentIndex: 0,
+            movementInterval: 100,
+            node
+        }
+
+        this.map.getRoot().addChild(node);
 //        const hunt = new Hunt(playerId);
 //        const run = new Run(playerId);
 //        const gridDefense = new GridDefense(playerId);
@@ -130,7 +250,11 @@ class VegasTrail extends Game {
     }
 
     tick() {
-        this.activeGame && this.activeGame.tick();
+        this.map.tick(this.playerStates);
+        this.drive.tick(this.playerStates);
+        this.hunt.tick(this.playerStates); 
+        this.fight.tick(this.playerStates);
+        this.stats.tick(this.playerStates); 
     }
 
     getLayers() {
--- src/games/vegas-trail/minigames/map.js
+++ src/games/vegas-trail/minigames/map.js
@@ -1,7 +1,8 @@
 const { Asset, Game, GameNode, Colors, Shapes, ShapeUtils, Physics, GeometryUtils, subtypes } = require('squish-0766');
 
 class MapGame {
-    constructor(mapData) {
+    constructor(mainGame, mapData) {
+        this.mainGame = mainGame;
         this.root = new GameNode.Shape({
             shapeType: Shapes.POLYGON,
             coordinates2d: ShapeUtils.rectangle(0, 0, 100, 100),
@@ -14,138 +15,30 @@ class MapGame {
     }
 
     constructMap(mapData) {
-        mapData = {
-            mapCoords: [
-                [92, 92],
-                [90, 85],
-                [85, 85],
-                [80, 80],
-                [75, 78],
-                [70, 78],
-                [70, 76],
-                [75, 72],
-                [75, 68],
-                [72, 66],
-                [71, 65],
-                [70, 64],
-                [69, 64],
-                [68, 64],
-                [67, 62],
-                [66, 57],
-                [66, 55],
-                [67, 54],
-                [68, 52],
-                [66, 48],
-                [66, 47],
-                [65, 45],
-                [63, 44],
-                [62, 43.5],
-                [60, 43],
-                [58, 42.5],
-                [56, 42.5],
-                [54, 44],
-                [52, 44],
-                [50, 44],
-                [47, 48],
-                [42, 48],
-                [38, 46],
-                [34, 45],
-                [32, 40],
-                [32, 36],
-                [31, 32],
-                [31, 26],
-                [30, 24],
-                [28, 22],
-                [26, 22],
-                [25, 21],
-                [23, 20.5],
-                [20, 20.5],
-                [16, 19],
-                [12, 19],
-                [11, 18],
-                [10, 14],
-                [9, 10],
-                [8, 8],
-                [8, 12],
-                [10, 18],
-                [11, 20],
-                [12, 21],
-                [16, 21],
-                [20, 22.5],
-                [23, 22.5],
-                [25, 23],
-                [26, 24],
-                [28, 24],
-                [30, 26],
-                [30, 28],
-                [30, 34],
-                [31, 38],
-                [32, 42],
-                [34, 47],
-                [38, 48],
-                [42, 50],
-                [47, 50],
-                [50, 46],
-                [52, 46],
-                [54, 46],
-                [56, 44.5],
-                [58, 44.5],
-                [60, 45],
-                [62, 45.5],
-                [63, 46],
-                [65, 47],
-                [66, 49],
-                [66, 50],
-                [67, 53],
-                [65, 54],
-                [65, 57],
-                [67, 64],
-                [68, 66],
-                [69, 66],
-                [70, 66],
-                [71, 67],
-                [72, 68],
-                [74, 70],
-                [72, 73],
-                [67, 78],
-                [70.5, 80],
-                [75, 81],
-                [80, 82],
-                [85, 87],
-                [92, 92]
-
-            //    [90, 85],
-            //    [85, 85],
-            //    [80, 80],
-            //    [75, 78],
-            //    [70, 78],
-            //    [70, 76],
-            //    [75, 72],
-            //    [75, 68],
-            //    [72, 66],
-            //    [71, 65],
-            //    [70, 64],
-            //    [69, 64],
-            //    [68, 64],
-            //    [67, 62],
-            //    [66, 57],
-            ],
-            landmarks: [
-                
-            ]
-        };
-
-        console.log("SEHE");
-        console.log(ShapeUtils.rectangle(0, 0, 10, 10));
-
         return new GameNode.Shape({
             shapeType: Shapes.POLYGON,
-            coordinates2d: mapData.mapCoords,//pathRectCoords,
+            coordinates2d: mapData.mapCoords,
             fill: Colors.COLORS.PINK
         });
     }
 
-    tick() {
+    tick(playerStates) {
+        const now = Date.now();
+        for (let key in playerStates) {
+            const playerState = playerStates[key];
+            if (!playerState.lastMovementTime || playerState.lastMovementTime + playerState.movementInterval <= now) {
+                if (playerState.currentIndex >= playerState.path.length) {
+                    continue;
+                }
+
+                const newCoords = ShapeUtils.rectangle(playerState.path[playerState.currentIndex][0], playerState.path[playerState.currentIndex][1], 2, 2);
+                // feels filthy but hey you know
+                playerStates[key].node.node.coordinates2d = newCoords;
+                playerStates[key].lastMovementTime = now;
+                playerStates[key].currentIndex = playerStates[key].currentIndex + 1;
+            }
+        }
+ 
     }
 
     getRoot() {
