cb7108ee0480898f8fc072bebf686b6f6e979bc2
Joseph Garcia <prosif@gmail.com>
2023-12-06 22:11:33 -0700

    ayy

--- src/games/clicker/index.js
+++ src/games/clicker/index.js
@@ -1,6 +1,24 @@
-const { Game, GameNode, Colors, Shapes } = require('squish-1005');
+const { Game, GameNode, Colors, Shapes, ShapeUtils } = require('squish-1005');
 
 const COLORS = Colors.COLORS;
+
+
+// TODO: add these to shapeutils
+
+const getCenter = (node) => {
+    return [
+        (node.node.coordinates2d[1][0] - node.node.coordinates2d[0][0]) / 2,
+        (node.node.coordinates2d[2][1] - node.node.coordinates2d[1][0]) / 2,
+    ]
+};
+
+const getXStart = (node) => node.node.coordinates2d[0][0];
+
+const getYStart = (node) => node.node.coordinates2d[0][1];
+
+const getHeight = (node) => node.node.coordinates2d[2][1] - node.node.coordinates2d[1][1];
+
+const getWidth = (node) => node.node.coordinates2d[1][0] - node.node.coordinates2d[0][0];
 
 const gameEntry = (x, y) => {
     const entry = new GameNode.Shape({
@@ -81,7 +99,12 @@ class Clicker extends Game {
         super();
 
         const baseColor = Colors.randomColor();
-        this.gameList = gameList();
+
+        this.rates = {};
+        this.progress = {};
+        this.progressNodes = {};
+
+        this.gameList = this.renderGameList();
        
         this.layers = [
             {
@@ -109,31 +132,76 @@ class Clicker extends Game {
             const game2 = gameEntry(20, 60);
             const game3 = gameEntry(20, 80);
 
-            this.gameExpireTimes = {
-                [game1.node.id]: Date.now() + 1000,
-                [game2.node.id]: Date.now() + 1000,
-                [game3.node.id]: Date.now() + 1000
-            };
+            this.rates[game1.node.id] = Math.random() * 10;//Date.now() + 1000,
+            this.rates[game2.node.id] = Math.random() * 10;
+            this.rates[game3.node.id] = Math.random() * 10;
+
+            this.progress[game1.node.id] = 0;
+            this.progress[game2.node.id] = 0;
+            this.progress[game3.node.id] = 0;
+
+            this.progressNodes[game1.node.id] = game1;
+            this.progressNodes[game2.node.id] = game2;
+            this.progressNodes[game3.node.id] = game3;
+
             return [game1, game2, game3];
         };
 
-        const renderedGames = this.renderedGames || initialGames();
-
-        console.log('rendered games!');
-        console.log(renderedGames);
-        console.log(this.gameExpireTimes);
+        const renderedGames = initialGames();
 
         gameListBase.clearChildren();
 
-        gameListBase.addChildren(renderedGames);
+        for (let i in renderedGames) {
+            gameListBase.addChild(renderedGames[i]);
+        }
 
-        this.gameListBase = gameListBase;
-        this.renderedGames = renderedGames;
+        return gameListBase;
+    }
+
+    onProgress(progressBarNodeId) {
+        const node = this.progressNodes[progressBarNodeId];
+
+        if (!node) {
+            console.warn('Attempted to render unknown progress bar node ' + progressBarNodeId);
+            return;
+        }
+
+        // basically guaranteed to be here
+        const textChild = node.node.children[0];
+        const textInfo = Object.assign({}, textChild.node.text);
+
+        const curPos = getCenter(node);
+        // todo: better names
+        const left = getXStart(node);
+        const _top = getYStart(node);
+        const nodeHeight = getHeight(node);
+        const nodeWidth = getWidth(node) + this.rates[progressBarNodeId];
+        if (nodeWidth > 80) {
+            this.gameList.removeChild(progressBarNodeId);
+            const newGame = gameEntry(left, _top);
+            this.rates[newGame.node.id] = Math.random() * 10;
+            this.progress[newGame.node.id] = 0;
+            this.progressNodes[newGame.node.id] = newGame;
+            this.gameList.addChild(newGame);
+
+            console.log('replacing node');
+
+            delete this.rates[progressBarNodeId];
+            // todo: free()
+        } 
+        const newCoords = ShapeUtils.rectangle(left, _top, nodeWidth, nodeHeight);
+        node.node.coordinates2d = newCoords; 
+        textInfo.text = 'ayy ' + nodeWidth;
+        textChild.node.text = textInfo;
     }
 
     tick() {
         if (!this.renderedGames || this.renderedGames.length < 3) {
-            this.renderGameList();
+//            this.renderGameList();
+        }
+        for (let gameNodeId in this.rates) {
+            this.onProgress(gameNodeId);
+//            console.log("ffoiofofof " + gameNodeId)
         }
     }
 
