f0db724f2d856c890e7cea214a3ef8c3825f6cda
Joseph Garcia <joseph@homegames.io>
2024-03-17 17:08:55 -0700

    Services (#95)
    * wip
    
    * cool
    
    * oh
    
    * cleanup

--- game_server.js
+++ game_server.js
@@ -1,4 +1,6 @@
 const GameSession = require('./src/GameSession');
+const http = require('http');
+const https = require('https');
 const { socketServer } = require('./src/util/socket');
 const Homenames = require('./src/Homenames');
 const path = require('path');
@@ -29,6 +31,87 @@ const server = (certPath, squishMap, username) => {
     // hack kind of. but homegames dashbaoard is special
     let session;
 
+    let services = {};
+
+    const supportedServices = {
+        'contentGenerator': {
+            requestContent: (request) => new Promise((resolve, reject) => {
+                const makePost = (path, _payload) => new Promise((resolve, reject) => {
+                    const payload = JSON.stringify(_payload);
+
+                    let module, hostname, port;
+                
+                    module = https;
+                    port = 443;
+                    hostname = 'api.homegames.io';
+                
+                    const headers = {};
+                
+                    Object.assign(headers, {
+                        'Content-Type': 'application/json',
+                        'Content-Length': payload.length
+                    });
+                
+                    const options = {
+                        hostname,
+                        path,
+                        port,
+                        method: 'POST',
+                        headers
+                    };
+                
+                    let responseData = '';
+                    
+                    const req = module.request(options, (res) => {
+                        res.on('data', (chunk) => {
+                            responseData += chunk;
+                        });
+                
+                        res.on('end', () => {
+                            resolve(responseData);
+                        });
+                    });
+                
+                    req.write(payload);
+                    req.end();
+                });
+
+                makePost('https://api.homegames.io/services', request).then((response) => {
+                    if (!response.startsWith('{')) {
+                        reject(response);
+                    } else {
+                        const requestId = JSON.parse(response).requestId;
+                        const interval = setInterval(() => {
+                            https.get(`https://api.homegames.io/service_requests/${requestId}`, {}, (res) => {
+                                let bufs = [];
+                                res.on('data', (chunk) => {
+                                    bufs.push(chunk);
+                                });
+
+                                res.on('end', () => {
+                                    const fin = JSON.parse(Buffer.concat(bufs));
+                                    const parsed = fin;//JSON.parse(fin);
+                                    if (parsed.response) {
+                                        clearInterval(interval);
+                                        resolve(parsed.response);
+                                    }
+                                });
+                            });
+                        }, 5000);
+                    }
+                }).catch(err => {
+                    reject(err);
+                });
+            })
+        }
+    };
+
+    if (customStartModule?.metadata) {
+        const requestedServices = customStartModule.metadata().services || [];
+        services = {};
+        requestedServices.forEach(s => services[s] = supportedServices[s]);
+    }
+
     const dashboard = customStartModule ? new customStartModule({ 
         squishMap,
         addAsset: (key, asset) => new Promise((resolve, reject) => {
@@ -37,7 +120,8 @@ const server = (certPath, squishMap, username) => {
             // }
         }),
         username,
-        certPath
+        certPath,
+        services
     }) : new HomegamesDashboard({ 
         squishMap, 
         movePlayer: (params) => {
@@ -49,7 +133,8 @@ const server = (certPath, squishMap, username) => {
             // }
         }),
         username,
-        certPath
+        certPath,
+        services
     });
     
     session = new GameSession(dashboard, HOME_PORT, username);
